6bf4fd7d0108f95d1440630551dbdbf0ba092465
Osman100 <36994519+Osman100@users.noreply.github.com>
2023-09-12 10:10:03 -0600

    CodeAdded

--- ProyectoLaFarmaciaFinal/DetalleTransaccion.cs
+++ ProyectoLaFarmaciaFinal/DetalleTransaccion.cs
@@ -12,7 +12,24 @@ namespace ProyectoLaFarmaciaFinal
         private int idTransaccion;
         private int idProducto;
         private int cantidadProducto;
-        private double subtotal;
+        private double subtotal; 
+
+        public DetalleTransaccion()
+        {
+        }
+
+        public int IdDetalle { get => idDetalle; set => idDetalle = value; }
+        public int IdTransaccion { get => idTransaccion; set => idTransaccion = value; }
+        public int IdProducto { get => idProducto; set => idProducto = value; }
+        public int CantidadProducto { get => cantidadProducto; set => cantidadProducto = value; }
+        public double Subtotal { get => subtotal; set => subtotal = value; }
+
+        public void calcularSubtotal()
+        {
+            throw new NotImplementedException();
+        }
 
     }
+
+
 }
