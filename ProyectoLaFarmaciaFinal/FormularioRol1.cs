4e511ed68e9219bd0017e6dca5228f4a2c3a1dbd
JessiMar <urbi.jessica26@hotmai.com>
2023-10-03 20:50:26 -0600

    AddingRol1

---
+++ ProyectoLaFarmaciaFinal/FormularioRol1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoLaFarmaciaFinal
+{
+    public partial class FormularioRol1 : Form
+    {
+        public FormularioRol1()
+        {
+            InitializeComponent();
+        }
+    }
+}
